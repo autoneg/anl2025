92f76f3def0513740bf6b5e25b5e098266f3d0dd
Yasser Mohammad <yasserfarouk@gmail.com>
2024-11-03 20:28:39 +0900

    Using better scenario generation

--- src/anl2025/anl2025.py
+++ src/anl2025/anl2025.py
@@ -1,4 +1,4 @@
-from random import randint, choice, random
+from random import choice, random
 from typing import Any
 import pandas as pd
 from rich import print
@@ -6,14 +6,16 @@ from pathlib import Path
 import typer
 from negmas.helpers.strings import unique_name
 from negmas.preferences import BaseUtilityFunction
-from negmas.preferences import LinearAdditiveUtilityFunction as U, UtilityFunction
+from negmas.preferences.generators import generate_multi_issue_ufuns
+from negmas.negotiators import ControlledNegotiator
+from negmas.preferences import UtilityFunction
 from negmas.sao.controllers import ControlledSAONegotiator, SAOController, SAOState
 from negmas import (
     DiscreteCartesianOutcomeSpace,
     ResponseType,
     SAOMechanism,
 )
-from negmas.outcomes import Outcome, make_issue, make_os
+from negmas.outcomes import Outcome
 from negmas.sao.negotiators import AspirationNegotiator
 from anl_agents import get_agents
 
@@ -98,16 +100,13 @@ class Boulware2025(ANL2025Negotiator):
     You can participate by an agent that runs any SAO negotiator independently for each thread.
     """
 
-    class MyNegotiator(AspirationNegotiator):
-        pass
-
     def __init__(self, **kwargs):
-        kwargs["default_negotiator_type"] = Boulware2025.MyNegotiator
+        kwargs["default_negotiator_type"] = AspirationNegotiator
         super().__init__(**kwargs)
 
 
 P_END = 0.03
-P_REJECT = 0.95
+P_REJECT = 0.999
 
 
 class RandomNegotiator(ANL2025Negotiator):
@@ -137,31 +136,44 @@ class RandomNegotiator(ANL2025Negotiator):
 def main(
     nedges: int = 3,
     nissues: int = 3,
+    nvalues: int = 7,
     nsteps: int = 100,
     center_reserved_value: float = 0.0,
     edge_reserved_value_min: float = 0.1,
-    edge_reserved_value_max: float = 0.6,
+    edge_reserved_value_max: float = 0.4,
     method: str = "serial",
     output: Path = Path.home() / "negmas" / "anl2025" / "session",
-    center_type: str = "RandomNegotiator",
+    center_type: str = "Boulware2025",
+    use_random: bool = True,
     use_anl2024: bool = False,
-    use_any_negotiator: bool = False,
-    dry: bool = False,
+    use_boulware: bool = True,
     keep_order: bool = False,
+    atomic: bool = False,
+    dry: bool = False,
 ):
-    os = make_os([make_issue(randint(3, 7)) for _ in range(nissues)])
+    ufuns = [generate_multi_issue_ufuns(nissues, nvalues) for _ in range(nedges)]
     d = edge_reserved_value_max - edge_reserved_value_min
-    edge_ufuns = [
-        U.random(os, reserved_value=random() * d + edge_reserved_value_min)
-        for _ in range(nedges)
-    ]
-    center_ufun = MaxCenterUFun(
-        ufuns=tuple(U.random(os) for _ in range(nedges)),
-        reserved_value=center_reserved_value,
-    )
+    edge_ufuns = [_[0] for _ in ufuns]
+    for u in edge_ufuns:
+        u.reserved_value = random() * d + edge_reserved_value_min
+    side_ufuns = tuple(_[1] for _ in ufuns)
+    # os = make_os([make_issue(randint(3, 7)) for _ in range(nissues)])
+    # edge_ufuns = [
+    #     U.random(os, reserved_value=random() * d + edge_reserved_value_min)
+    #     for _ in range(nedges)
+    # ]  # tuple(U.random(os) for _ in range(nedges)),
+    center_ufun = MaxCenterUFun(ufuns=side_ufuns, reserved_value=center_reserved_value)
+
+    def type_name(x):
+        if isinstance(x, Boulware2025):
+            return "Boulware2025"
+        if not issubclass(x.default_negotiator_type, ControlledNegotiator):
+            return f"ANL2025({x.default_negotiator_type.__name__})"
+        return x.__class__.__name__.split(".")[-1]
+
     center = eval(center_type)(id="center", ufun=center_ufun)
-    print(f"Adding center of type {center.__class__.__name__}")
-    agents = [RandomNegotiator]  # type: ignore
+    print(f"Adding center of type {type_name(center)}")
+    agents = [RandomNegotiator] if use_random else []  # type: ignore
     if use_anl2024:
         agents += [
             lambda ufun, id: ANL2025Negotiator(
@@ -174,7 +186,7 @@ def main(
             )
             for _ in get_agents(2024, finalists_only=True, as_class=True)
         ]
-    if use_any_negotiator:
+    if use_boulware:
         agents.append(Boulware2025)  # type: ignore
     mechanisms: list[SAOMechanism] = []
     edges: list[ANL2025Negotiator] = []
@@ -182,10 +194,13 @@ def main(
         edge = choice(agents)(ufun=edge_ufun, id=f"edge{i}")
         edges.append(edge)
         m = SAOMechanism(
-            outcome_space=os, one_offer_per_step=True, name=f"n{i}", n_steps=nsteps
+            outcome_space=edge_ufun.outcome_space,
+            one_offer_per_step=atomic,
+            name=f"n{i}",
+            n_steps=nsteps,
         )
         print(
-            f"Adding edge {i} of type {edge.__class__.__name__} (thread: {m.name} ID: {m.id})"
+            f"Adding edge {i} of type {type_name(edge)} (thread: {m.name} ID: {m.id})"
         )
         m.add(
             center.create_negotiator(
@@ -211,15 +226,15 @@ def main(
     SAOMechanism.runall(mechanisms, method=method, keep_order=keep_order)  # type: ignore
     base = output / unique_name("session", sep=".")
     base.mkdir(parents=True, exist_ok=True)
-    print(f"Center Utility: {center_ufun(tuple(_.agreement for _ in mechanisms))}")
     for i, (e, m, u) in enumerate(zip(edges, mechanisms, center_ufun.ufuns)):
         print(
-            f"Mechanism {m.name} ({m.id}) between ({m.negotiator_ids}) ended with {m.agreement}: "
+            f"Mechanism {m.name} ({m.id}) between ({m.negotiator_ids}) ended in {m.current_step} ({m.relative_time:4.3}) with {m.agreement}: "
             f"Edge Utility = {e.ufun(m.agreement) if e.ufun else 'unknown'}, "
             f"Side Utility = {u(m.agreement) if u else 'unknown'}"
         )
         df = pd.DataFrame(data=m.full_trace, columns=TRACE_COLS)  # type: ignore
         df.to_csv(base / f"neg{i}-{m.id}.csv", index_label="index")
+    print(f"Center Utility: {center_ufun(tuple(_.agreement for _ in mechanisms))}")
 
 
 if __name__ == "__main__":
